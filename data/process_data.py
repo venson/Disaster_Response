3580fcf56113b341d5088608ff2118dbc6b9f197
venson <venson.hunt@gmail.com>
2021-07-06 14:39:06 +0800

    [doc]add docstring and comments

--- data/process_data.py
+++ data/process_data.py
@@ -1,63 +1,109 @@
+# import
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
+
 def load_data(messages_filepath, categories_filepath):
-    # read file
+    """
+    Load data messages and categories form csv file.
+
+    Parameters
+    ----------
+        messages_filepath : path to messages csv file
+        categories_filepath : path to categories csv file
+
+    Returns
+    ----------
+        df : DataFrame thich have both messages and categories infomations.
+    """
+    # read files
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     # merge datasets on id
-    df = messages.merge(categories,on = 'id')
+    df = messages.merge(categories, on='id')
     return df
 
+
 def clean_data(df):
-    
-    #split categories 
-    categories = pd.DataFrame(df.categories.str.split(';',expand = True))
+    """
+    Clean the dataframe create from load_data()
+    separate categories from text into each feature per column
+
+    Parameters
+    --------
+        df : DataFrame need to be cleaned.
+
+    Returns
+    --------
+        df :DataFrame after cleaning.
+
+    """
+    #split categories
+    categories = pd.DataFrame(df.categories.str.split(';', expand=True))
     # use the first row to name the columns
-    row = categories.iloc[0,:]
+    row = categories.iloc[0, :]
     # use the words except the last 2 characters as columns names
     category_colnames = row.apply(lambda x: x[:-2])
     # rename the columns of `categories`
     categories.columns = category_colnames
-    
+
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].str[-1]
         # convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
-        
+
     # drop the original categories column from `df`
-    df.drop(['categories'], axis = 1, inplace = True)
-    # concatenate the processed info together as new 'df' 
-    df = pd.concat([df, categories], axis = 1)
+    df.drop(['categories'], axis=1, inplace=True)
+    # concatenate the processed info together as new 'df'
+    df = pd.concat([df, categories], axis=1)
     # remove duplicates
     df = df.drop_duplicates()
     return df
-    
+
+
 def save_data(df, database_filename):
+    """
+    Save Cleaned dataframe in sqlite database file with table "DisasterResponse"
     
-    engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('DisasterResponse', engine, index=False)  
+    Parameters
+    ----------
+        df: dataframe need to be saved
+        database_filename : path to sqlite database file
+
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False)
 
 
 def main():
+
+    ## run with the command have 4 argument values
     if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        ## get argument values to variables
+        ## 1st argument value : path of messages csv file to be loaded
+        ## 2nd argument value : path of categories csv file to be loaded
+        ## 3rd argument value : path of sqlite database file to be saved to
+        messages_filepath, categories_filepath, database_filepath = sys.argv[
+            1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        #load csv file to dataframe
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(
+            messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
 
+        # clean the dataset
         print('Cleaning data...')
         df = clean_data(df)
-        
+
+        # save the cleaned dataframe to sqlite database file
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -4,10 +4,9 @@ import sys
 import nltk
 import pandas as pd
 
-nltk.download(['punkt', 'wordnet'])
 import pickle
 
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import RidgeClassifier
 from sklearn.metrics import classification_report
@@ -15,9 +14,24 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sqlalchemy import create_engine
+# comment the next line out if run under archlinux
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 
 
 def load_data(database_filepath):
+    """
+    Load database from "DisasterResponse" table from sqlite database file.
+
+    Parameters
+    -----------
+    database_filepath : File path to sqlite database file.
+
+    Returns 
+    ----------
+    X : Texts from database for modeling, need process before modeling.
+    y : category labels for the texts, for modeling, 36 columns total.
+    category_names : the category names for y ,used for model evaluation.
+    """
     # create engine for database file
     engine = create_engine('sqlite:///' + database_filepath)
     # read table
@@ -31,66 +45,146 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    """
+    Used as tokenizer for CountVectorizer with word_tokenize() and WordNetlemmatizer() from nltk package.
+    The words will be lemmatized with WordNet dict. 
+
+    Parameters:
+    ----------
+        text : String needed to be tokenized.
+
+    Returns
+    ----------
+        token_list: a list of words after tokenized and lemmatized.
+
+
+
+
+    """
+
+    ## tokenize all the text
     tokens = nltk.word_tokenize(text)
+    ## init WordNetLemmatizer
     lemmatizer = nltk.WordNetLemmatizer()
 
+    ## create blank list for return
     token_list = []
+    ## lemmatize all the tokenized words that is not a stopword
     for token in tokens:
-        tk = lemmatizer.lemmatize(token).lower().strip()
-        token_list.append(tk)
+        if token not in nltk.stopwords("english"):
+            tk = lemmatizer.lemmatize(token).lower().strip()
+            token_list.append(tk)
 
     return token_list
 
 
 def build_model():
+    """
+    build model pipeline for disaster dataset.
+    1. CountVectorizer
+    2. TfidfTransformer
+    3. MultiOutputClassifier with RidgeClassifier
+    4. GridSearchCV for tunning(sv = 3, 4 cpu cores)
+
+    Parameters
+    ---------
+       
+    Returns
+    ---------
+        cv : GridSearchCV model
+
+    """
+
+    #model pipeline for GridSearchCV
     pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
                          ('tfidf', TfidfTransformer()),
                          ('clf', MultiOutputClassifier(RidgeClassifier()))])
+    ## param_grid for GridSearchCV
     parameters = {
         'vect__ngram_range': ((1, 1), (1, 2)),
         'vect__max_df': (0.5, 0.75, 1),
         'vect__max_features': (None, 5000, 10000),
         'clf__estimator__alpha': (0.5, 0.75, 1)
     }
+    ## GridSearchCV with cv = 3 and cpu 4 cores for shortter time in fiting
     cv = GridSearchCV(pipeline, param_grid=parameters, cv=3, n_jobs=4)
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Evaluate the model provided.
+    Reports each kind of categorys predict accuracy, f1 scores.
+
+    Parameters
+    ----------
+        model : model need to be evaluated.
+        X_test : X for model to predict from
+        Y_test : Y for comparison with model predict values
+        category_names: names of each columns of Y
+
+
+    """
+    # predict with the model and X data provided
     y_pred = model.predict(X_test)
+    # use the prediction to build a dataframe
     y_pred_df = pd.DataFrame(y_pred, columns=category_names)
+
+    # caculate model scores for each category
     for column in category_names:
-        print('Report for {} :'.format(column))
+        print('>>>Report for {} :'.format(column))
         print(
-            classification_report(Y_test[column],
-                                  y_pred_df[column],
-                                  zero_division=0))
-        print('--------------------------------')
+            classification_report(
+                pd.DataFrame(Y_test[column],
+                             y_pred_df[column],
+                             zero_division=0)))
+        print('-------------')
 
 
 def save_model(model, model_filepath):
+    """
+    Save the model to a PKL file.
+
+    Parameters
+    ------------
+        model : model that needed to be saved.
+        model_filepath : the path where the model will be save to.
+
+        
+    """
+    # open a file connection and save the model
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 
 
 def main():
+    # run while the execute command has 3 argument values.
     if len(sys.argv) == 3:
+        # the second argument value is database filepath
+        # the third argument value is model_filepath
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        #load information from database file
         X, Y, category_names = load_data(database_filepath)
+
+        # split train and test data set for model
         X_train, X_test, Y_train, Y_test = train_test_split(X,
                                                             Y,
                                                             test_size=0.2)
 
+        # build model.
         print('Building model...')
         model = build_model()
 
+        # train model with GridSearchCV
         print('Training model...')
         model.fit(X_train, Y_train)
 
+        # evaluating the model accuracy
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
+        # save model to PKL file
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
 
